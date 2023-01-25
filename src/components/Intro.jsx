34b0c23aca0d4fa5b17fef2f95463e9c1d56e4da
Prince Singh <princesingh808087@gmail.com>
2023-01-25 17:52:05 +0530

    created landing profile

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,52 +2,18 @@ import React from "react";
 
 const Intro = () => {
 	return (
-		<div className=" text-white">
-			<div>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates minima, cum blanditiis
-				distinctio illo accusamus vitae nam aut mollitia? Animi laudantium corrupti minima dolorum
-				deleniti doloribus modi, fugit ab magnam.
-			</div>
-			<div>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates minima, cum blanditiis
-				distinctio illo accusamus vitae nam aut mollitia? Animi laudantium corrupti minima dolorum
-				deleniti doloribus modi, fugit ab magnam.
-			</div>
-			<div>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates minima, cum blanditiis
-				distinctio illo accusamus vitae nam aut mollitia? Animi laudantium corrupti minima dolorum
-				deleniti doloribus modi, fugit ab magnam.
-			</div>
-			<div>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates minima, cum blanditiis
-				distinctio illo accusamus vitae nam aut mollitia? Animi laudantium corrupti minima dolorum
-				deleniti doloribus modi, fugit ab magnam.
-			</div>
-			<div>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates minima, cum blanditiis
-				distinctio illo accusamus vitae nam aut mollitia? Animi laudantium corrupti minima dolorum
-				deleniti doloribus modi, fugit ab magnam.
-			</div>
-			<div>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates minima, cum blanditiis
-				distinctio illo accusamus vitae nam aut mollitia? Animi laudantium corrupti minima dolorum
-				deleniti doloribus modi, fugit ab magnam.
-			</div>
-			<div>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates minima, cum blanditiis
-				distinctio illo accusamus vitae nam aut mollitia? Animi laudantium corrupti minima dolorum
-				deleniti doloribus modi, fugit ab magnam.
-			</div>
-			<div>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates minima, cum blanditiis
-				distinctio illo accusamus vitae nam aut mollitia? Animi laudantium corrupti minima dolorum
-				deleniti doloribus modi, fugit ab magnam.
-			</div>
-			<div>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates minima, cum blanditiis
-				distinctio illo accusamus vitae nam aut mollitia? Animi laudantium corrupti minima dolorum
-				deleniti doloribus modi, fugit ab magnam.
-			</div>
+		<div className="flex flex-col px-10 h-screen justify-center md:ml-40">
+			<h3 className="text-textColor md:text-xl">Hi,</h3>
+			<h3 className="text-textColor md:text-xl">My name is,</h3>
+			<h1 className="text-slate-300 text-3xl md:text-7xl font-semibold"> Prince Singh.</h1>{" "}
+			<span className="text-slate-400 text-xl md:text-3xl font-medium">Front end engineer</span>
+			<h3 className="text-slate-400 text-xl md:text-3xl font-medium ">
+				I build applications for the web.
+			</h3>
+			<p className="text-lg md:text-xl text-slate-300 mt-12 md:w-80">
+				I'm a frontend engineer specializing in building accessible and user facing features with
+				the help of reusable codes
+			</p>
 		</div>
 	);
 };
