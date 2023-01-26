afede0e118ae6d20ac69cd9c63d9fe4740fa2d5f
Prince Singh <princesingh808087@gmail.com>
2023-01-26 23:37:19 +0530

    other projects section completed

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -54,6 +54,55 @@ export const Data = [
 export const otherData = [
 	{
 		title: "Todo List",
-		description: "",
+		github: "https://github.com/PrinceSingh19/TodoList",
+		demo: "https://todoprince.netlify.app/",
+		description: "A simple todo list app where you can create, modify, and delete todos",
+		tech: "React, Sass",
+		imgSrc: "../../public/images/todo.jpg",
+	},
+
+	{
+		title: "Ristaurante Con Fusion",
+		description: "A static restaurant website",
+		tech: "JS, Gulp, Gulp plugin, BootStrap, CSS",
+		imgSrc: "../../public/images/restaurante.jpg",
+		demo: "https://princesingh19.github.io/BootStrap-build-with-Gulp-task-runner/dist/",
+		github: "https://github.com/PrinceSingh19/BootStrap-build-with-Gulp-task-runner",
+	},
+	{
+		title: "Dam Website",
+		description: "A static dam website",
+		tech: "HTML5, CSS3, BootStrap",
+		imgSrc: "../../public/images/dam.jpg",
+		demo: "https://princesingh19.github.io/Web-Development-Projects/Hirakud%20Dam%20Website/index.html",
+		github:
+			"https://github.com/PrinceSingh19/Web-Development-Projects/tree/main/Hirakud%20Dam%20Website",
+	},
+	{
+		title: "Product Landing Page",
+		description: "A product landing page",
+		tech: "HTML5, CSS3",
+		imgSrc: "../../public/images/landing.jpg",
+		demo: "https://princesingh19.github.io/Web-Development-Projects/Product%20Landing%20Page/",
+		github:
+			"https://github.com/PrinceSingh19/Web-Development-Projects/tree/main/Product%20Landing%20Page",
+	},
+	{
+		title: "Technical Documentation Page",
+		description:
+			"A responsive technical documentation page to understand data binding and navigation to various parts using HTML only",
+		tech: "HTML5, CSS3",
+		imgSrc: "../../public/images/documentation.jpg",
+		demo: "https://princesingh19.github.io/Web-Development-Projects/Technical%20Documentation%20Page/",
+		github:
+			"https://github.com/PrinceSingh19/Web-Development-Projects/tree/main/Technical%20Documentation%20Page",
+	},
+	{
+		title: "Survey Form",
+		description: "A responsive survey form",
+		tech: "HTML5, CSS3",
+		imgSrc: "../../public/images/survey.jpg",
+		demo: "https://princesingh19.github.io/Web-Development-Projects/Survey%20Form/",
+		github: "https://github.com/PrinceSingh19/Web-Development-Projects/tree/main/Survey%20Form",
 	},
 ];
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import SingleProject from "./SingleProject";
-import { Data } from "./Data";
+import { Data, otherData } from "./Data";
 import OtherProjects from "./OtherProjects";
 
 const Projects = () => {
+	const mobileOther = otherData.slice(0, 3);
 	return (
 		<div className="w-3/4 mx-auto mt-20 md:mt-40">
 			<hr className="text-textColor w-1/2" />
 			<div className="mt-2">
+				{/* Displaying main react projects */}
 				<h1 className="text-slate-300 text-2xl font-medium mb-4">
 					Project that I have made recently
 				</h1>
@@ -17,8 +19,21 @@ const Projects = () => {
 					})}
 				</div>
 				<div>
+					{/* Displaying other small projects */}
 					<h1 className="text-textColor">Other noteworthy projects</h1>
-					<OtherProjects />
+
+					<div className="grid grid-cols-1 gap-y-3 gap-x-7 mb-7 mt-4 md:hidden">
+						{/* Displaying only 3 projects on mobile devices */}
+						{mobileOther.map((other, index) => {
+							return <OtherProjects key={index} other={other} />;
+						})}
+					</div>
+					<div className="hidden md:grid gap-y-3 grid-cols-3 gap-x-7 mb-7 mt-4 ">
+						{/* Displaying all other projects on md devices */}
+						{otherData.map((other, index) => {
+							return <OtherProjects key={index} other={other} />;
+						})}
+					</div>
 				</div>
 			</div>
 		</div>
